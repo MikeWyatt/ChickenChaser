e0c355a75f3a384e4c18e7bb3d69c0516e87f762
2DArray <2DArray@gmail.com>
2013-12-08 08:56:12 -0800

    New models/textures, chicken updates

---
+++ Assets/Plugins/Scripts/Chicken/WobbleAnim.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+private var timer:float=0f;
+
+function Start () {
+	
+}
+
+function Update () {
+	timer+=Time.deltaTime*15f;
+	transform.localRotation=Quaternion.Euler(-90f+Mathf.Sin(timer)*15f,90f,0f);
+}
