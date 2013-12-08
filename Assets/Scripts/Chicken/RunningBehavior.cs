e4b1c350a03283472383b372c14697fae404da45
Mike Wyatt <mikejohnwyatt@gmail.com>
2013-12-07 23:15:43 -0800

    RunningBehavior is now smoother

--- Assets/Scripts/Chicken/RunningBehavior.cs
+++ Assets/Scripts/Chicken/RunningBehavior.cs
@@ -7,7 +7,8 @@ public class RunningBehavior : MonoBehaviour, IChickenBehavior {
 	public float Speed = 5f;
 	public float EscapeRadius = 15f;
 	public float PlayerAvoidanceWeight = 4f;		// how badly the chicken wants to avoid the players vs. reaching the fence
-	public float PlayerAvoidanceRadius = 3f;
+	public float PlayerAvoidanceMinRadius = 1.5f;	// should be chicken radius + ChickenScarer radius
+	public float PlayerAvoidanceMaxRadius = 5f;		// distance at which chicken starts avoiding the ChickenScarer.
 
 	void Update () {
 		Vector3 deltaFromOrigin = (transform.position - FindObjectOfType<ChickenSpawner>().transform.position);
@@ -23,17 +24,19 @@ public class RunningBehavior : MonoBehaviour, IChickenBehavior {
 
 		// find nearby players
 		IEnumerable<GameObject> nearbyPlayers = FindObjectsOfType<ChickenScarer>()
-			.Select(pc => pc.gameObject)
-			.Where(p => (p.transform.position - transform.position).magnitude <= PlayerAvoidanceRadius);
+			.Select(pc => pc.gameObject);
 
 		// add bias so chicken moves away from players.
 		foreach(GameObject go in nearbyPlayers) {
-			desired += -(go.transform.position - transform.position).normalized * PlayerAvoidanceWeight;
+			Vector3 delta = go.transform.position - transform.position;
+			float weight = 1f - Mathf.Clamp01((delta.magnitude - PlayerAvoidanceMinRadius) / PlayerAvoidanceMaxRadius);
+			float finalWeight = Mathf.Pow (weight, 4) * PlayerAvoidanceWeight;
+			desired += -delta.normalized * finalWeight;
 		}
 
 		desired.Normalize();
 
 		transform.position += desired * Speed * Time.deltaTime;
-		transform.rotation = Quaternion.LookRotation(desired);  // not working
+		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desired), 8 * Time.deltaTime);
 	}
 }
