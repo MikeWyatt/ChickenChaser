5763b2e149bab09858b82123c6d4d4cb35c92751
Mike Wyatt <mikejohnwyatt@gmail.com>
2013-12-07 17:31:08 -0800

    Chicken FSM changes, Chicken prefab, changed camera angle

---
+++ Assets/Scripts/Chicken/IChickenBehavior.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Marker interface for chicken behaviors.
+/// </summary>
+public interface IChickenBehavior {
+	bool enabled { get; set; }
+}
--- Assets/Scripts/Chicken/ThrownBehavior.cs
+++ Assets/Scripts/Chicken/ThrownBehavior.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ThrownBehavior : MonoBehaviour {
+public class ThrownBehavior : MonoBehaviour, IChickenBehavior {
 
 	// Use this for initialization
 	void Start () {
