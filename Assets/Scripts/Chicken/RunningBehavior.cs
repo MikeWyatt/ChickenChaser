14ea1903879174a039828952d72f179e0a3eb62c
Mike Wyatt <mikejohnwyatt@gmail.com>
2013-12-07 21:35:32 -0800

    RunningBehavior works nicely now

--- Assets/Scripts/Chicken/RunningBehavior.cs
+++ Assets/Scripts/Chicken/RunningBehavior.cs
@@ -5,44 +5,36 @@ using System.Linq;
 
 public class RunningBehavior : MonoBehaviour, IChickenBehavior {
 	public float Speed = 5f;
-
-	private GameObject[] escapeZones;
-	public string EscapeZoneTag = "Escape Zone";
-
-	void Start () {
-		escapeZones = FindObjectsOfType<Collider>()
-			.Select(c => c.gameObject)
-			.Where(go => go.CompareTag(EscapeZoneTag))
-			.ToArray();
-
-		if(escapeZones.Count() == 0) {
-			throw new UnityException(string.Format ("No colliders found with tag {0}!", EscapeZoneTag));
-		}
-	}
+	public float EscapeRadius = 15f;
+	public float PlayerAvoidanceWeight = 4f;		// how badly the chicken wants to avoid the players vs. reaching the fence
+	public float PlayerAvoidanceRadius = 3f;
+	public GameObject Origin;	// fixed point chicken is running from, other than the players
 
 	void Update () {
-		Vector3 escapePoint = CalcEscapePoint();
+		Vector3 deltaFromOrigin = (transform.position - Origin.transform.position);
 
-		var delta = escapePoint - transform.position;
-		delta.Normalize();
-		transform.position += delta * Speed * Time.deltaTime;
-		transform.LookAt(escapePoint);
-	}
-
-	public Vector3 CalcEscapePoint() {
-		var options = escapeZones.ToDictionary(
-			go => go,
-			go => go.collider.ClosestPointOnBounds(transform.position));
-
-		KeyValuePair<GameObject, Vector3> closest = options.OrderBy(kvp => (transform.position - kvp.Value).sqrMagnitude).First();
-		return closest.Value;
-	}
-
-	public void OnCollisionEnter(Collision collision) {
-		if(!collision.gameObject.CompareTag(EscapeZoneTag)) {
+		// check if escaped
+		if(deltaFromOrigin.magnitude >= EscapeRadius) {
+			Destroy(gameObject);
 			return;
 		}
-		//GetComponent<Chicken>().ChangeBehavior<RunningBehavior>();
-		Destroy(gameObject);
+
+		// start desired vector so that chicken simply tries to leave the map
+		Vector3 desired = deltaFromOrigin.normalized;
+
+		// find nearby players
+		IEnumerable<GameObject> nearbyPlayers = FindObjectsOfType<ChickenScarer>()
+			.Select(pc => pc.gameObject)
+			.Where(p => (p.transform.position - transform.position).magnitude <= PlayerAvoidanceRadius);
+
+		// add bias so chicken moves away from players.
+		foreach(GameObject go in nearbyPlayers) {
+			desired += -(go.transform.position - transform.position).normalized * PlayerAvoidanceWeight;
+		}
+
+		desired.Normalize();
+
+		transform.position += desired * Speed * Time.deltaTime;
+		transform.rotation = Quaternion.LookRotation(desired);  // not working
 	}
 }
